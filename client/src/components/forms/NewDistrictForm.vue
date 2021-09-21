13ac18f009d7ca3efe9b664e5487fab703569d40
karkaplani <ilgu0001@algonquinlive.com>
2021-09-21 17:09:24 -0400

    district adding works

--- client/src/components/forms/NewDistrictForm.vue
+++ client/src/components/forms/NewDistrictForm.vue
@@ -1,14 +1,41 @@
 <template>
-  <form>
-    <label for="club-name">District Name</label> <br>
-    <input type="text"> <br> <br>
-    <button>Submit</button>
-  </form>
+  <div>
+    <form>
+      <label for="club-name">District Name</label> <br>
+      <input type="text"
+        v-model="districtName"> <br> <br>
+      <button @click="addNewDistrict">Submit</button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
+  name: 'NewDistrictForm',
+  components: {
 
+  },
+  data() {
+    return {
+      districtName: '',
+      // more will be added
+    }
+  },
+  methods: {
+    async addNewDistrict(event) {
+      event.preventDefault()
+      
+      let districtToAdd = {
+        name: this.districtName
+      }
+
+      const res = await fetch('/api/districts', { 
+        method: 'POST', 
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(districtToAdd)})
+      console.log(res)
+    }
+  }
 }
 </script>
 
