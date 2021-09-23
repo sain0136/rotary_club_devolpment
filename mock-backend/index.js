1a4f511debcf9ef8484d0944fca172188e5cc8b2
karkaplani <ilgu0001@algonquinlive.com>
2021-09-23 13:30:01 -0400

    backend updated with clubs api

--- mock-backend/index.js
+++ mock-backend/index.js
@@ -1,4 +1,4 @@
-const districts = [
+let districts = [
   {
     id: 1,
     name: 'D1'
@@ -10,6 +10,21 @@ const districts = [
   {
     id: 3,
     name: 'D3'
+  }
+]
+
+let clubs = [
+  {
+    id: 1,
+    name: 'C1'
+  },
+  {
+    id: 2,
+    name: 'C2'
+  },
+  {
+    id: 3,
+    name: 'C3'
   }
 ]
 
@@ -33,13 +48,44 @@ app.get('/districts/:id', (req, res) => {
 })
 
 app.post('/districts', (req, res) => {
-  console.log(req.body)
   let newDistrict = {
     id: Date.now(),
     name: req.body.name
   }
   districts.push(newDistrict)
   res.send(newDistrict)
+})
+
+app.delete('/districts/:id', async (req, res) => {
+  districts = districts.filter(district => district.id != req.params.id)
+  res.send(districts)
+})
+
+app.get('/clubs', (req, res) => {
+  res.send(clubs)
+})
+
+app.get('/clubs/:id', (req, res) => {
+  clubs.forEach(club => {
+    if(club.id == req.params.id) {
+      res.send(club)
+    }
+  })
+})
+
+app.post('/clubs', (req, res) => {
+  console.log(req.body)
+  let newClub = {
+    id: Date.now(),
+    name: req.body.name
+  }
+  districts.push(newClub)
+  res.send(newClub)
+})
+
+app.delete('/clubs/:id', async (req, res) => {
+  clubs = clubs.filter(club => club.id != req.params.id)
+  res.send(clubs)
 })
 
 const PORT = process.env.PORT || 5000
