bf54e15188df7f6f75f22d51da46b0345463c924
sain0136 <55252053+sain0136@users.noreply.github.com>
2021-10-16 20:43:04 -0400

    second typescript error

--- app/Modules/Project/routes.ts
+++ app/Modules/Project/routes.ts
@@ -1,5 +1,4 @@
 import Route from '@ioc:Adonis/Core/Route'
-import ProjectsController from 'App/Controllers/Http/ProjectsController'
 
 Route.resource('/project/', 'ProjectsController')
 Route.post('/project/addUser/', 'ProjectsController.addUserToProject')
