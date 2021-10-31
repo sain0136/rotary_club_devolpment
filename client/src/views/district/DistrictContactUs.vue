f59eaa04e510b4baa81b046ca77496ae508d53ab
karkaplani <ilgu0001@algonquinlive.com>
2021-10-30 21:24:27 -0400

    district contact us added

--- client/src/views/district/DistrictContactUs.vue
+++ client/src/views/district/DistrictContactUs.vue
@@ -1,13 +1,54 @@
 <template>
-  <div>
-    District Contact Us
+  <div class="container">
+    <h3>Contact Information</h3> <br>
+    <div class="description-container">
+      Address: {{this.districtAddress}} <br>
+      Email: {{this.districtEmail}}
+    </div>
   </div>
 </template>
 
 <script>
 
+import store from '../../store/index'
+
 export default {
-  name: 'DistrictContactUs'
+  name: 'DistrictContactUs',
+  data() {
+    return {
+      districtEmail: '',
+      districtAddress: ''
+    }
+  },
+  methods: {
+    async fetchDistrictContactInfo() {
+      const res = await fetch(`/api/district/${store.state.currentDistrictId}`, {
+        method: 'GET'
+      })
+      const data = await res.json()
+      const districtInfo = await data.districtById
+      return {
+        email: await districtInfo.district_email,
+        address: await districtInfo.meeting_location
+      }
+    }
+  },
+  async created() {
+    const contactInfo = await this.fetchDistrictContactInfo()
+    
+    this.districtEmail = await contactInfo.email
+    this.districtAddress = await contactInfo.address
+  }
 }
 
 </script>
+
+<style scoped>
+
+.container {
+  text-align: center;
+  width: 50%;
+  word-wrap: break-word;
+}
+
+</style>
