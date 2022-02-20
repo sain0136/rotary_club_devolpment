00a5560c5f368ae201a0cbfe769dfd6b0bc30227
sain0136 <55252053+sain0136@users.noreply.github.com>
2022-02-20 12:45:06 -0500

    api correction #2

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,8 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '^/api': {
-        target:
-          'http://127.0.0.1:9222/',
+        target: 'http://localhost:9222',
         changeOrigin: true,
         logLevel: 'debug',
         pathRewrite: { '^/api': '/' },
