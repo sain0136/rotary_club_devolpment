42c9f01e5ed3d1c3f09e6ee8221cbfd9b228e9ab
sain0136 <55252053+sain0136@users.noreply.github.com>
2022-05-19 11:30:37 -0400

    error control

--- app/Controllers/Http/MailController.ts
+++ app/Controllers/Http/MailController.ts
@@ -2,27 +2,26 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Mail from '@ioc:Adonis/Addons/Mail'
 
 export default class MailController {
-  public async contactUsFormMail({ request,response }: HttpContextContract) {
+  public async contactUsFormMail({ request, response }: HttpContextContract) {
     const senderEmail: string = request.input('sender_email')
     const senderName: string = request.input('sender_name')
     const body: string = request.input('body')
     try {
       await Mail.send((message) => {
-      message
-        .from('jssr26@gmail.com')
-        .to('jssr26@gmail.com')
-        .replyTo(senderEmail)
-        .subject('Rotary Enquiry from:' + senderName)
-        .html(
-          'Click link to reply or just click reply to reply to the actual sender: ' +
-            senderEmail +
-            '. ' +
-            body
-        )
-    })
+        message
+          .from('jssr26@gmail.com')
+          .to('jssr26@gmail.com')
+          .replyTo(senderEmail)
+          .subject('Rotary Enquiry from:' + senderName)
+          .html(
+            'Click link to reply or just click reply to reply to the actual sender: ' +
+              senderEmail +
+              '. ' +
+              body
+          )
+      })
     } catch (error) {
-      return response.json({error:'your email failed'})
+      return response.json({ error: 'your email failed' + error })
     }
-    
   }
 }
