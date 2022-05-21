668052169a1555dfde2b3a9f37d2745edb5fbb95
sain0136 <55252053+sain0136@users.noreply.github.com>
2022-05-20 20:14:18 -0400

    ee

--- config/cors.ts
+++ config/cors.ts
@@ -44,7 +44,7 @@ const corsConfig: CorsConfig = {
   |                     one of the above values.
   |
   */
-  origin:'http://127.0.0.1:7984',
+  origin: true,
 
   /*
   |--------------------------------------------------------------------------
